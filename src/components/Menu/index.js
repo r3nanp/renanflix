639f2eb902a9eb87fe0aea5120fc032d1dd8c791
Renan Pereira <renanmol87@gmail.com>
2020-07-27 17:02:30 -0300

    initial commit.

---
+++ src/components/Menu/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Logo from '../../assets/img/Logo.png';
+import './styles.css';
+
+function Menu() {
+  return(
+  <nav ClassName="Menu">
+    <a>
+
+    </a>
+    <img src ={Logo} class="Logo" alt="RenanFlixlogo" />
+  </nav>
+
+  );
+}
+
+export default Menu;
+
