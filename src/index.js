a3b534c54311b3ceb1b959f09a513b94c08021b9
Renan Pereira <renanmol87@gmail.com>
2020-07-29 23:47:16 -0300

    feat: adição de na página de 404, haver um jogo

--- src/index.js
+++ src/index.js
@@ -10,13 +10,21 @@ import Home from './pages/Home';
 import Video from './pages/Cadastro/Video';
 import Categoria from './pages/Cadastro/Categoria';
 
+const error = () => (
+  <div> 404 
+    <a href="https://mariosouto.com/flappy-bird-devsoutinho/">
+      Página não encontrada
+    </a>
+  </div>
+);
+
 ReactDOM.render(
   <Router>
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={Video} />
       <Route path="/cadastro/categoria" component={Categoria} />
-      <Route component={() => (<h1>404</h1>)} />
+      <Route component={error} />
     </Switch>
   </Router>,
   document.getElementById('root')
