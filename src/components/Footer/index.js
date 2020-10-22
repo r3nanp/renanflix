dcdbe061dcac31a6548aac0901128e38116f2623
Renan Pereira <renanmol87@gmail.com>
2020-10-22 14:44:05 -0300

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,12 +4,11 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://linkedin.com/in/renan-pereira-968bb61b0">
+      <a href="https://linkedin.com/in/r3nanp">
         <img src='https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/linkedin.svg' alt="Logo"/>
       </a>
       <p>
         Orgulhosamente criado durante a
-        {' '}
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
         </a>
