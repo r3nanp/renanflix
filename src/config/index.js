0edbaff552f4599441c2f322d3449c7089eb794f
r3nanp <renanmol87@gmail.com>
2020-10-28 13:45:29 -0300

    refactor (index): remove the comma and semi

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 /* eslint-disable linebreak-style */
 const URL = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://renanflix.herokuapp.com';
+  : 'https://renanflix.herokuapp.com'
 
 export default {
   URL,
-};
+}
